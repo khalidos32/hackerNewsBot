b7d3d03d52478f8349708b0c7833e6d59d038385
khalid <khalidos321@gmail.com>
2019-07-23 21:56:09 +0100

    fix bug done

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.get('/', async (req, res) => {
     let articles = await axios.get(TOP_NEWS).then(response=>response.data)
     articles = articles.slice(0,1)
     articles.forEach(article=>promises.push(sendArticle(article.url)))
-    await Promise.all(promises)
-    return res.json(articles)  
+    await Promise.all(promises).catch(err => {
+        console.log(err)
+    })
+    return res.json({done:true})
 })
 
 app.listen('8080',()=>{
